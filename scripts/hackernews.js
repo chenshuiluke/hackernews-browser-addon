66716f2a947b3347051512c4b0760ce0fac8adfb
Luke Chen Shui <chenshuiluke@gmail.com>
2017-10-03 13:41:21 -0500

    Functional addon

---
+++ scripts/hackernews.js
@@ -0,0 +1,17 @@
+function request(){
+
+}
+$.get("https://ng-hackernews.lukecs.com/api/stories/top", function(data){
+    console.log(data);
+    var container = $('#item-list');
+    var to_add = [];
+    for(var counter = 0; counter < data.length; counter++){
+        var item = data[counter];
+        var new_list_item = "<a href=\"" + item.url + "\">" + item.title + "</a>";
+        to_add.push(new_list_item);
+    }
+    container.append(to_add);
+})
+.fail(function(){
+    console.log("Error loading stories from ng-hackernews.lukecs.com!");
+});
